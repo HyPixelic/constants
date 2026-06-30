79e42ec33ff8286fcf317e95fed382ba7b784503
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2026-06-30 17:06:53 +0200

    Reworked folder structure for now

--- packages/static/index.ts
+++ packages/static/index.ts
@@ -1,12 +1,12 @@
-import GAMES from "../../constants/games.json" with { type: "json" };
-import ACHIEVEMENTS from "../../constants/achievements.json" with { type: "json" };
-import GUILD_ACHIEVEMENTS from "../../constants/guildAchievements.json" with { type: "json" };
-import QUESTS from "../../constants/quests.json" with { type: "json" };
-import CHALLENGES from "../../constants/challenges.json" with { type: "json" };
-import VANITY_PETS from "../../constants/vanityPets.json" with { type: "json" };
-import VANITY_COMPANIONS from "../../constants/vanityCompanions.json" with { type: "json" };
-import SKYBLOCK_COLLECTIONS from "../../constants/skyblock/collections.json" with { type: "json" };
-import SKYBLOCK_ITEMS from "../../constants/skyblock/items.json" with { type: "json" };
+import GAMES from "./constants/games.json" with { type: "json" };
+import ACHIEVEMENTS from "./constants/achievements.json" with { type: "json" };
+import GUILD_ACHIEVEMENTS from "./constants/guildAchievements.json" with { type: "json" };
+import QUESTS from "./constants/quests.json" with { type: "json" };
+import CHALLENGES from "./constants/challenges.json" with { type: "json" };
+import VANITY_PETS from "./constants/vanityPets.json" with { type: "json" };
+import VANITY_COMPANIONS from "./constants/vanityCompanions.json" with { type: "json" };
+import SKYBLOCK_COLLECTIONS from "./constants/skyblock/collections.json" with { type: "json" };
+import SKYBLOCK_ITEMS from "./constants/skyblock/items.json" with { type: "json" };
 
 export {
   GAMES,
