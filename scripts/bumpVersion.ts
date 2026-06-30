17a281acde7d6d8389019e643751180284e97801
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2026-06-30 18:00:34 +0200

    Limit to one version per day

--- scripts/bumpVersion.ts
+++ scripts/bumpVersion.ts
@@ -11,22 +11,7 @@ try {
 
   const dateVersion = `${new Date().getFullYear()}.${new Date().getMonth() + 1}.${new Date().getDate()}`;
 
-  let newVersion = dateVersion;
-
-  if (oldVersion.startsWith(dateVersion)) {
-    if (oldVersion === dateVersion) {
-      newVersion = `${dateVersion}-1`;
-    } else {
-      const match = oldVersion.match(/-(\d+)$/);
-      if (match) {
-        newVersion = `${dateVersion}-${Number(match[1]) + 1}`;
-      } else {
-        newVersion = `${dateVersion}-1`;
-      }
-    }
-  }
-
-  JSRFile.version = newVersion;
+  JSRFile.version = dateVersion;
 
   writeFileSync(path, JSON.stringify(JSRFile, null, 2) + "\n", "utf8");
   logger.info("BumpVersion", `Bumped version from ${oldVersion} to ${JSRFile.version}`);
