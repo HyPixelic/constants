ac2f12633b09d3697dbc507672513281545704c8
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2026-07-01 10:48:46 +0200

    Reworked live package to traverse object if only one key

--- packages/live/index.ts
+++ packages/live/index.ts
@@ -19,24 +19,32 @@ type Constant =
  * @example const skyblockItems = constants.SKYBLOCK_ITEMS
  */
 // @ts-expect-error
-const constants: Record<Constant, object> = {};
+const constants: Record<Constant, any> = {};
 
-const resources: Record<Constant, string> = {
-  GAMES: "https://api.hypixel.net/v2/resources/games",
-  ACHIEVEMENTS: "https://api.hypixel.net/v2/resources/achievements",
+type Resource = string | string[];
+
+const resources: Record<Constant, Resource> = {
+  GAMES: ["https://api.hypixel.net/v2/resources/games", "games"],
+  ACHIEVEMENTS: ["https://api.hypixel.net/v2/resources/achievements", "achievements"],
   GUILD_ACHIEVEMENTS: "https://api.hypixel.net/v2/resources/guilds/achievements",
-  QUESTS: "https://api.hypixel.net/v2/resources/quests",
-  CHALLENGES: "https://api.hypixel.net/v2/resources/challenges",
+  QUESTS: ["https://api.hypixel.net/v2/resources/quests", "quests"],
+  CHALLENGES: ["https://api.hypixel.net/v2/resources/challenges", "challenges"],
   VANITY_PETS: "https://api.hypixel.net/v2/resources/vanity/pets",
   VANITY_COMPANIONS: "https://api.hypixel.net/v2/resources/vanity/companions",
-  SKYBLOCK_COLLECTIONS: "https://api.hypixel.net/v2/resources/skyblock/collections",
-  SKYBLOCK_ITEMS: "https://api.hypixel.net/v2/resources/skyblock/items",
+  SKYBLOCK_COLLECTIONS: ["https://api.hypixel.net/v2/resources/skyblock/collections", "collections"],
+  SKYBLOCK_ITEMS: ["https://api.hypixel.net/v2/resources/skyblock/items", "items"],
 };
 
 const fetchResources = async (): Promise<void> => {
   for (const [key, value] of Object.entries(resources) as [Constant, string][]) {
     try {
-      constants[key] = await (await fetch(value)).json();
+      if (Array.isArray(value)) {
+        constants[key] = (await (await fetch(value[0])).json())[value[1]];
+      } else {
+        constants[key] = await (await fetch(value)).json();
+      }
+      delete constants[key]["success"];
+      delete constants[key]["lastUpdated"];
     } catch {
       console.log("Failed to fetch resource.");
     }
